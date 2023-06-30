81443291c334743966daeb397d7f366fb535162d
Jeigsaw <sumanull@outlook.com>
2023-06-30 14:33:15 +0545

    vite in project

--- main.js
+++
@@ -1,5 +0,0 @@
-window.onload = (event) => {
-  let el = document.querySelector('.stuffCard img');
-  const angle = 20 - Math.random() * 40
-  el.style.setProperty('--tiltAngle', angle +'deg');
-};
---
+++ src/depends.ts
@@ -0,0 +1,5 @@
+import Alpine from 'alpinejs';
+
+
+// alpine init
+Alpine.start();
